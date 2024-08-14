4710e1994882deda5493f24cb45e4a3217950f8b
= <madebyallanbahr@gmail.com>
2024-08-14 20:25:59 -0300

    feat: adiciona exercicio 6

---
+++ exercises/06/solution.js
@@ -0,0 +1,12 @@
+/**
+ * @description Write a function that 
+ * create a string from getting 
+ * the first 3 characters and the 3 last characters
+ * @function createNewStringFromReceived(str)
+ */
+
+const createNewStringFromReceived = (string) => {
+ if (string.length < 3) return string
+};
+
+console.log(createNewStringFromReceived());
