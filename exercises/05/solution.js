95f7f35ff2d47016ce6fb05c9813d6326c4bb242
= <madebyallanbahr@gmail.com>
2024-08-14 20:11:39 -0300

    feat: completed exercise 05

---
+++ exercises/05/solution.js
@@ -0,0 +1,13 @@
+/**
+ * @description Write a function that put in front a string the word
+ * "New!" if str already have return it self.
+ * @function returnNewInFrontString(str)
+ */
+
+const returnNewInFrontString = (string) => {
+  if (!string) return null;
+  if (string.includes("New!")) return string;
+  return string = "New!" + string;
+};
+
+console.log(returnNewInFrontString("JustDoIt"));
