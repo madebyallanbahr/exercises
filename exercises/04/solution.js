49534fbf13625d36de08db1015cb54e6c7ff7704
= <madebyallanbahr@gmail.com>
2024-08-14 12:25:49 -0300

    feat: new exercise /04

---
+++ exercises/04/solution.js
@@ -0,0 +1,11 @@
+/**
+ * @description Write a function 
+ * that returns the current date
+ * @function getCurrentDate 
+ */
+
+const getCurrentDate = () => {
+    // todo
+};
+
+console.log(getCurrentDate())
